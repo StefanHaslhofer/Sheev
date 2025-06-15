f638d0b6cfa4991c056d55865ecd888bae9f76a2
StefanHaslhofer <haslhofer.stefan@gmail.com>
2025-06-15 13:04:35 +0200

    add sheev slave

--- sheev/sheev.ino
+++ sheev/sheev.ino
@@ -1,70 +1,35 @@
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+#include "TOF_Sense.h"
+#include <AFMotor.h>
 
-#define DOOR_PIN D7  // pin connected to door sensor's pin
+// setup motors
+AF_DCMotor m1(1, MOTOR12_1KHZ);
+AF_DCMotor m2(2, MOTOR12_1KHZ);
+AF_DCMotor m3(3, MOTOR34_1KHZ);
+AF_DCMotor m4(4, MOTOR34_1KHZ);
 
-#define DEBUG_MSG true
+void setup(){
+  Serial.begin(115200);
 
-const char* ssid     = "Des Kaisers Richtfunk";
-const char* password = "GottbewahredenKaiser";
-
-const unsigned int NET_PORT = 58266;
-WiFiUDP Udp;
-IPAddress broadcastIP;
-
-int door_state; // current state of door sensor
-int prev_door_state;    // previous state of door sensor
-
-void setup() {
-  Serial.begin(9600);
-  delay(10);
-  pinMode(DOOR_PIN, INPUT_PULLUP);
-  door_state = digitalRead(DOOR_PIN); // read state
-
-  Serial.println('\n');
+  m1.setSpeed(200);
+  m2.setSpeed(200);
+  m3.setSpeed(200);
+  m4.setSpeed(200);
   
-  WiFi.begin(ssid, password);             // connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.print(" ...");
-
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // wait for the Wi-Fi to connect
-    delay(1000);
-    Serial.print("...");
-  }
-  Serial.println(" connection established!");  
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());
-
-  // calculate last address in current subnet (= broadcast address) by bitwise operation
-  broadcastIP = ~(uint32_t)WiFi.subnetMask() | (uint32_t)WiFi.gatewayIP();
-  Serial.print("Broadcast IP: ");
-  Serial.println(broadcastIP);
+  Wire.begin();
+  Wire.setClock(400000);
 }
 
-void loop() {
-  prev_door_state = door_state; // save the last state
-  door_state  = digitalRead(DOOR_PIN); // read new state
-
-  if (prev_door_state == LOW && door_state == HIGH) { // state change: LOW -> HIGH
-    #if DEBUG_MSG
-      Serial.println("The door-opening has been detected");  
-    #endif
-    broadcast("opened");
-  }
-  else
-  if (prev_door_state == HIGH && door_state == LOW) { // state change: HIGH -> LOW
-    #if DEBUG_MSG
-      Serial.println("The door-closing has been detected");
-    #endif
-    broadcast("closed");
-  }
-
+void loop(){
+  Serial.print("tick");
+  motor.run(FORWARD); // Motor goes forward
   delay(1000);
-}
+  Serial.print("tock");
+  motor.run(BACKWARD); // Motor goes backwards
+  delay(1000);
+  Serial.print("tack");
+  motor.run(RELEASE); // Motor stops
+  delay(1000);
 
-void broadcast(char msg[]){
-  Udp.beginPacket(broadcastIP, NET_PORT);
-  Udp.write(msg);
-  Udp.endPacket();
+  TOF_Inquire_I2C_Decoding();
+  delay(20);
 }
