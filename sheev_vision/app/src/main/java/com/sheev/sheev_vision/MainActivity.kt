61171b99809344c12d764740e68f68a7a6271d36
StefanHaslhofer <haslhofer.stefan@gmail.com>
2025-06-22 21:24:36 +0200

    add activity main binding

--- sheev_vision/app/src/main/java/com/sheev/sheev_vision/MainActivity.kt
+++ sheev_vision/app/src/main/java/com/sheev/sheev_vision/MainActivity.kt
@@ -3,8 +3,11 @@ package com.sheev.sheev_vision
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import com.sheev.sheev_vision.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
