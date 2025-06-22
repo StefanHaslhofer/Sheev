ad812af7b01cbc4a6d21b136925c848245249d3c
StefanHaslhofer <haslhofer.stefan@gmail.com>
2025-06-22 21:11:47 +0200

    add coordinator layout to main activity

--- sheev_vision/app/src/main/java/com/sheev/sheev_vision/MainActivity.kt
+++ sheev_vision/app/src/main/java/com/sheev/sheev_vision/MainActivity.kt
@@ -2,46 +2,12 @@ package com.sheev.sheev_vision
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.sheev.sheev_vision.ui.theme.Sheev_visionTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Sheev_visionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Sheev_visionTheme {
-        Greeting("Android")
     }
 }
