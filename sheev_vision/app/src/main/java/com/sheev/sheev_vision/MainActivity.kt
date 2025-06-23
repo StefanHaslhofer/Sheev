5ee87de7ea2aa55c80b28e9e1c9e632fcbb68e25
StefanHaslhofer <haslhofer.stefan@gmail.com>
2025-06-23 20:53:57 +0200

    working app

--- sheev_vision/app/src/main/java/com/sheev/sheev_vision/MainActivity.kt
+++ sheev_vision/app/src/main/java/com/sheev/sheev_vision/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.sheev.sheev_vision
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import com.sheev.sheev_vision.databinding.ActivityMainBinding
@@ -12,5 +14,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        var arr: Array<String> = arrayOf("123", "TEST")
+        val broadcastMsgAdapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, arr)
+
+        val listView: ListView = findViewById(R.id.broadcast_msg_view)
+        listView.adapter = broadcastMsgAdapter
     }
 }
